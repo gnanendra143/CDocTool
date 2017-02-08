7609876cbb214d218938e1994bffdc8542823df6
Yadav <GB043621@cerner.net>
2017-02-08 19:55:39 +0530

    Refactor code.

--- DesignDocument/DesignDocument/BrowseDocument.cs
+++ DesignDocument/DesignDocument/BrowseDocument.cs
@@ -9,29 +9,15 @@ namespace DesignDocument
         public static string getFilePath()
         {
             string filePath = string.Empty;
-            OpenFileDialog bDialog = new OpenFileDialog();
-            try
+            using (OpenFileDialog bDialog = new OpenFileDialog())
             {
                 bDialog.Title = "Open Word File";
                 bDialog.Filter = "Doc Files|*.doc;*.docx";//specifying which documents to load
                 bDialog.InitialDirectory = @"C:\Documents\";//specifying directory
-                //when clicked ok get theselected file name
+                                                            //when clicked ok get theselected file name
                 if (bDialog.ShowDialog() == DialogResult.OK)
                 {
                     filePath = bDialog.FileName.ToString();
-                }
-                
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
-            }
-            finally
-            {
-                if (null != bDialog)
-                {
-                    bDialog.Dispose();
-                    bDialog = null;
                 }
             }
             return filePath;
